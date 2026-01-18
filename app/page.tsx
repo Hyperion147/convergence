05c171e9710dba19f58af81b66d0b230b1109ca0
Hyperion147 <hypercode9913@gmail.com>
2026-01-18 22:47:54 +0530

    added classes and bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,19 @@
-import { Convergence } from "convergence";
-import Image from "next/image";
+import { TextHoverEffect } from "@/components/ui/text-hover-effect";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-        <Convergence />
-      </main>
+    <div className="flex flex-col gap-8 items-center justify-center font-sans bg-background">
+      <div className="max-w-5xl text-foreground text-7xl font-bold tracking-wide flex flex-col items-center cursor-default pt-20">
+        <h2 className="bg-linear-to-r from-emerald-400 via-emerald-800 to-emerald-400 bg-clip-text text-transparent line leading-normal">
+          Convergence-UI
+        </h2>
+        <p className="w-xl h-2 bg-linear-to-r from-emerald-800 via-emerald-400 to-emerald-800 rounded-sm"></p>
+      </div>
+      <div className="flex flex-col items-center gap-4">
+        <p className="bg-background opacity-80 border px-4 rounded-sm">Demo website for convergence</p>
+        <p className="text-xl text-accent tracking-wide">Transform your UI with custom color themes in real-time.</p>
+      </div>
+      <TextHoverEffect text="CONVERGENCE" />
     </div>
   );
 }
