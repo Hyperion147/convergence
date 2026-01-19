82f411d89db924c3cfcc4be77cafe9258c5cea43
Hyperion147 <hypercode9913@gmail.com>
2026-01-19 21:58:44 +0530

    fixed build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Convergence } from "@/.yalc/convergence/dist";
+import { Convergence } from "convergence-ui";
 import ClickSpark from "@/components/ClickSpark";
 import { Toaster } from "react-hot-toast";
 
